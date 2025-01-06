d14effae544d82aec14d106cc3c24ed80f16b902
piumal302 <piumal302@gmail.com>
2025-01-06 11:22:56 +0530

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { PaperProvider, Text, Divider, Button } from "react-native-paper";
 export default function Home() {
   const navigation = useNavigation();
